4e4c920ab1f503512ed7807fdfc649fab82b0863
manoj7019 <124152401+manoj7019@users.noreply.github.com>
2025-05-06 17:10:05 +0530

    small changes

--- projects.js
+++ projects.js
@@ -34,6 +34,13 @@ document.getElementById('INMP').addEventListener('mousemove', function() {
     document.getElementById('map-project-card-container-gj').classList.remove('map-project-card-containers');
     // mapClick.play();
 })
+
+document.getElementById('INMP').addEventListener('mouseleave', function() {
+    document.getElementById('map-default-projects-info-right').style.display = 'flex';
+    document.getElementById('map-project-card-container-mp').classList.remove('map-project-card-containers');
+    document.getElementById('INMP').style.fill = ' var(--stateColor)';
+})
+
 document.getElementById('INMP').addEventListener('mousemove', function() {
     document.getElementById('INMP').style.fill = 'rgb(0, 60, 0)';
     // document.getElementById('INMP').style.filter = 'drop-shadow(0 0 5px #4dff56) drop-shadow(0 0 5px #4dff56) drop-shadow(0 0 20px #4dff56)';
@@ -66,6 +73,12 @@ document.getElementById('INUP').addEventListener('mousemove', function() {
     document.getElementById('INGJ').style.fill = 'var(--stateColor)';
 })
 
+document.getElementById('INUP').addEventListener('mouseleave', function() {
+    document.getElementById('map-default-projects-info-right').style.display = 'flex';
+    document.getElementById('map-project-card-container-up').classList.remove('map-project-card-containers');
+    document.getElementById('INUP').style.fill = ' var(--stateColor)';
+})
+
 document.getElementById('INHR').addEventListener('mousemove', function() {
     document.getElementById('map-default-projects-info-right').style.display = 'none';
     document.getElementById('map-project-card-container-hr').classList.add('map-project-card-containers');
@@ -85,6 +98,11 @@ document.getElementById('INHR').addEventListener('mousemove', function() {
     document.getElementById('INCT').style.fill = 'var(--stateColor)';
     document.getElementById('INRJ').style.fill = 'var(--stateColor)';
     document.getElementById('INGJ').style.fill = 'var(--stateColor)';
+})
+document.getElementById('INHR').addEventListener('mouseleave', function() {
+    document.getElementById('map-default-projects-info-right').style.display = 'flex';
+    document.getElementById('map-project-card-container-hr').classList.remove('map-project-card-containers');
+    document.getElementById('INHR').style.fill = ' var(--stateColor)';
 })
 
 document.getElementById('INAP').addEventListener('mousemove', function() {
@@ -107,6 +125,11 @@ document.getElementById('INAP').addEventListener('mousemove', function() {
     document.getElementById('INRJ').style.fill = 'var(--stateColor)';
     document.getElementById('INGJ').style.fill = 'var(--stateColor)';
 })
+document.getElementById('INAP').addEventListener('mouseleave', function() {
+    document.getElementById('map-default-projects-info-right').style.display = 'flex';
+    document.getElementById('map-project-card-container-ap').classList.remove('map-project-card-containers');
+    document.getElementById('INAP').style.fill = ' var(--stateColor)';
+})
 
 document.getElementById('INCT').addEventListener('mousemove', function() {
     document.getElementById('map-default-projects-info-right').style.display = 'none';
@@ -127,6 +150,11 @@ document.getElementById('INCT').addEventListener('mousemove', function() {
     document.getElementById('INHR').style.fill = 'var(--stateColor)';
     document.getElementById('INRJ').style.fill = 'var(--stateColor)';
     document.getElementById('INGJ').style.fill = 'var(--stateColor)';
+})
+document.getElementById('INCT').addEventListener('mouseleave', function() {
+    document.getElementById('map-default-projects-info-right').style.display = 'flex';
+    document.getElementById('map-project-card-container-ct').classList.remove('map-project-card-containers');
+    document.getElementById('INCT').style.fill = ' var(--stateColor)';
 })
 
 document.getElementById('INRJ').addEventListener('mousemove', function() {
@@ -149,6 +177,11 @@ document.getElementById('INRJ').addEventListener('mousemove', function() {
     document.getElementById('INHR').style.fill = 'var(--stateColor)';
     document.getElementById('INGJ').style.fill = 'var(--stateColor)';
 })
+document.getElementById('INRJ').addEventListener('mouseleave', function() {
+    document.getElementById('map-default-projects-info-right').style.display = 'flex';
+    document.getElementById('map-project-card-container-rj').classList.remove('map-project-card-containers');
+    document.getElementById('INRJ').style.fill = ' var(--stateColor)';
+})
 
 document.getElementById('INGJ').addEventListener('mousemove', function() {
     document.getElementById('map-default-projects-info-right').style.display = 'none';
@@ -169,6 +202,11 @@ document.getElementById('INGJ').addEventListener('mousemove', function() {
     document.getElementById('INCT').style.fill = 'var(--stateColor)';
     document.getElementById('INRJ').style.fill = 'var(--stateColor)';
     document.getElementById('INHR').style.fill = 'var(--stateColor)';
+})
+document.getElementById('INGJ').addEventListener('mouseleave', function() {
+    document.getElementById('map-default-projects-info-right').style.display = 'flex';
+    document.getElementById('map-project-card-container-gj').classList.remove('map-project-card-containers');
+    document.getElementById('INGJ').style.fill = ' var(--stateColor)';
 })
 
 
@@ -191,6 +229,11 @@ document.getElementById('INUP2').addEventListener('mousemove', function() {
     document.getElementById('INAP2').style.fill = 'var(--stateColor)';
     document.getElementById('INCT2').style.fill = 'var(--stateColor)';
 })
+document.getElementById('INUP2').addEventListener('mouseleave', function() {
+    document.getElementById('map-igrpl-projects-info-right').style.display = 'flex';
+    document.getElementById('map-project-card-container-up2').classList.remove('map-project-card-containers');
+    document.getElementById('INUP2').style.fill = ' var(--stateColor)';
+})
 
 document.getElementById('INHR2').addEventListener('mousemove', function() {
     document.getElementById('map-igrpl-projects-info-right').style.display = 'none';
@@ -205,6 +248,11 @@ document.getElementById('INHR2').addEventListener('mousemove', function() {
     document.getElementById('INUP2').style.fill = 'var(--stateColor)';
     document.getElementById('INAP2').style.fill = 'var(--stateColor)';
     document.getElementById('INCT2').style.fill = 'var(--stateColor)';
+})
+document.getElementById('INHR2').addEventListener('mouseleave', function() {
+    document.getElementById('map-igrpl-projects-info-right').style.display = 'flex';
+    document.getElementById('map-project-card-container-hr2').classList.remove('map-project-card-containers');
+    document.getElementById('INHR2').style.fill = ' var(--stateColor)';
 })
 
 document.getElementById('INAP2').addEventListener('mousemove', function() {
@@ -221,6 +269,12 @@ document.getElementById('INAP2').addEventListener('mousemove', function() {
     document.getElementById('INHR2').style.fill = 'var(--stateColor)';
     document.getElementById('INCT2').style.fill = 'var(--stateColor)';
 })
+document.getElementById('INAP2').addEventListener('mouseleave', function() {
+    document.getElementById('map-igrpl-projects-info-right').style.display = 'flex';
+    document.getElementById('map-project-card-container-ap2').classList.remove('map-project-card-containers');
+    document.getElementById('INAP2').style.fill = ' var(--stateColor)';
+})
+
 
 document.getElementById('INCT2').addEventListener('mousemove', function() {
     document.getElementById('map-igrpl-projects-info-right').style.display = 'none';
@@ -235,6 +289,11 @@ document.getElementById('INCT2').addEventListener('mousemove', function() {
     document.getElementById('INUP2').style.fill = 'var(--stateColor)';
     document.getElementById('INHR2').style.fill = 'var(--stateColor)';
     document.getElementById('INAP2').style.fill = 'var(--stateColor)';
+})
+document.getElementById('INCT2').addEventListener('mouseleave', function() {
+    document.getElementById('map-igrpl-projects-info-right').style.display = 'flex';
+    document.getElementById('map-project-card-container-ct2').classList.remove('map-project-card-containers');
+    document.getElementById('INCT2').style.fill = ' var(--stateColor)';
 })
 
 // function clickSound() {
